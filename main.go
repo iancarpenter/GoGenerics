83a12a0a53c8671e0364dc813b192c1aaeae84c5
ICY <ICY@blah.com>
2025-04-14 22:06:05 +0100

    Add generic GetType function to determine the type of input values

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"reflect"
 )
 
+// Generic function that returns the type of the input value
+func GetType[T any](value T) string {
+	return reflect.TypeOf(value).String()
+}
+
 // Generic function that works with any type
 func HelloWorld[T any](value T) {
 	fmt.Printf("Hello, %v! (Type: %s)\n", value, reflect.TypeOf(value))
@@ -15,4 +20,9 @@ func main() {
 	HelloWorld(123)     // Using an integer
 	HelloWorld(3.14)    // Using a float
 	HelloWorld(true)    // Using a boolean
+
+	theType := GetType("Hello")
+	fmt.Printf("The type of 'Hello' is: %s\n", theType)
+	theType = GetType(42)
+	fmt.Printf("The type of 42 is: %s\n", theType)
 }
